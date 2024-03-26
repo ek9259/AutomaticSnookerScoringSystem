04c477f64f1ef7336edae7d4144c85024a75e937
Edmund Kong <edmundkong9259@gmail.com>
2024-03-27 02:39:39 +0800

    FIxing bugs

---
+++ SnookerScoringSystem.UseCases/GetVideoPathUseCase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SnookerScoringSystem.UseCases.Interfaces;
+using SnookerScoringSystem.UseCases.PluginInterfaces;
+
+namespace SnookerScoringSystem.UseCases
+{
+    public class GetVideoPathUseCase : IGetVideoPathUseCase
+    {
+        private readonly IVideoProcessingRepository _videoProcessingRepository;
+
+        public GetVideoPathUseCase(IVideoProcessingRepository videoProcessingRepository)
+        {
+            this._videoProcessingRepository = videoProcessingRepository;
+        }
+        public string Execute()
+        {
+            return _videoProcessingRepository.GetVideoPath();
+        }
+    }
+}
